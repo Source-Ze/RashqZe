5ed3dd401facd76fc66f4ea9df0c236eeaadf46b
Mody <143101587+Source-Ze@users.noreply.github.com>
2023-11-06 16:29:41 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,9 @@
-# تم برمجة البوت بواسطة https://t.me/EK_N1
-# Leader of the Seven Eyes Organization 
-# ممنوع منعا باتا تغيير الحقوق تتهان حبي 
 from config import Config
 import pyrogram , pyromod
 from pyromod import listen
 from pyrogram import Client, filters, enums
 from kvsqlite.sync import Client as dt
-p = dict(root='RashqElhakeM')
+p = dict(root='RashqMody')
 tok = Config.TG_BOT_TOKEN ## توكنك 
 id = Config.APP_ID ## ايديك حبي
 db = dt("data.sqlite", 'fuck')
@@ -15,7 +12,7 @@ if not db.get("admin_list"):
 if not db.get('ban_list'):
   db.set('ban_list', [])
 if not db.get('force'):
-  db.set('force', ['se7en_eyes'])
+  db.set('force', ['UI_XB])
 x = Client(name='RashqZe', api_id=Config.APP_ID, api_hash=Config.API_HASH, bot_token=tok, workers=20, plugins=p, parse_mode=enums.ParseMode.DEFAULT)
 
 x.run()
